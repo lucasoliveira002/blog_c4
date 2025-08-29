a95bed0b442784fd80d30d17c1a76381c1ed6119
Lucas Shimoda <135052326+lucasoliveira002@users.noreply.github.com>
2025-08-28 21:35:29 -0300

    Introdução do JavaScript para gerenciar posts
    Nesse Commit estou adicionando um sistema em JS para gerenciar os posts mais recentes que serão exibidos em index.html.
    Importante avisar que eu não sei como a imagem dos posts estão sendo puxadas, mas estão funcionando

---
+++ scripts/posts.js
@@ -0,0 +1,41 @@
+const posts = [
+    {
+        titulo: "First Post",
+        resumo: "This is the content of the first post.",
+        link: "post1.html",
+        data: "2025-08-01",
+        imagem: "first-post.jpg"
+    },
+    {
+        titulo: "Uma breve história do servidor de minecraft com meus  amiguinhos",
+        resumo: "Nesse post você irá descobrir a história do servidor de minecraft que tive com alguns amigos e todas as confusões que teve nele.",
+        link: "post2.html",
+        data: "2025-08-28",
+        imagem: "../images/icons/jogos.jpg"
+    },
+    // Adicione mais posts aqui
+];
+
+posts.sort((a, b) => new Date(b.data) - new Date(a.data));
+const recentes = posts.slice(0, 3);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const postsDiv = document.querySelector('.posts');
+    postsDiv.innerHTML = '';
+    recentes.forEach(post => {
+        postsDiv.innerHTML += `
+            <article class="post">
+                <div class="conteudo-post">
+                    <div class="imagem-post">
+                        <img src="images/${post.imagem.replace('.html', '.jpg')}" alt="${post.imagem}" width="100" height="100">
+                    </div>
+                    <div class="texto-post">
+                        <h2>${post.titulo}</h2>
+                        <p>${post.resumo}</p>
+                        <a href="${post.link}">Read more</a>
+                    </div>
+                </div>                
+            </article>
+        `; //Eu não sei como a imagem está sendo chamada (??????) Se tá funcionando, não mexe
+    });
+});
